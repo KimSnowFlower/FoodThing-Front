dfa93b2f580cfde5ea495c98eb0c2db1835acc89
KimSnowFlower <kimbona1148@gmail.com>
2025-11-09 19:42:15 +0900

    feat: #4 add style variables, base layout for MainPage, and image assets

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// components
+import TopBar from '../../components/TopBar';
+import TabBar from '../../components/TabBar';
+
+// lib api
+// import api from '../../lib/api';
+
+// css
+import "../MainPage/MainPage.module.css";
+
+// assets
+import search_icon from '../../assets/images/search_icon.svg';
+import send_icon from '../../assets/images/arrow_circle_icon.svg';
+import today_what_eat_icon from '../../assets/images/today_what_eat_icon.png';
+
+const MainPage = () => {
+
+
+}
+
+export default MainPage;
